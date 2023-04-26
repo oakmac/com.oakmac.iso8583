bdaa8f96135944ccf18c061b6a2702d1d3be01cf
Aaron Walker <aaron@treasuryprime.com>
2023-04-26 12:55:08 -0400

    add a test

--- test/com/oakmac/iso8583/writer_test.clj
+++ test/com/oakmac/iso8583/writer_test.clj
@@ -1,9 +1,9 @@
 (ns com.oakmac.iso8583.writer-test
   (:require
-    [clojure.test :refer [deftest is testing]]
-    [com.oakmac.iso8583.binary :as binary]
-    [com.oakmac.iso8583.format-iso8583 :as format-iso8583]
-    [com.oakmac.iso8583.writer :as writer]))
+   [clojure.test :refer [deftest is testing]]
+   [com.oakmac.iso8583.binary :as binary]
+   [com.oakmac.iso8583.format-iso8583 :as format-iso8583]
+   [com.oakmac.iso8583.writer :as writer]))
 
 (deftest write-test
   (testing "Can write the message-type"
@@ -21,9 +21,12 @@
 (deftest write-fields-test
   (testing "Can write a few fields"
     (is (= (binary/bytes-to-hex
-             (writer/write (format-iso8583/field-definitions)
-               {:message-type "0200"
-                :pan "1111222233334444"
-                :processing-code "011000"
-                :transaction-amount "000000006660"}))
-           (str (binary/bytes-to-hex "0200") "7000000000000000" (binary/bytes-to-hex "161111222233334444011000000000006660"))))))
+            (writer/write (format-iso8583/field-definitions)
+                          {:message-type "0200"
+                           :pan "1111222233334444"
+                           :processing-code "011000"
+                           :transaction-amount "000000006660"
+                           ;; This is a 3 digit variable length field with
+                           ;; length 17. Expect it to be 0 padded to 017.
+                           :message-reason-code "because i said so"}))
+           (str (binary/bytes-to-hex "0200") "7000000000000000" (binary/bytes-to-hex "161111222233334444011000000000006660017because i said so"))))))
